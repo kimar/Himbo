aeafeafa64d3381e029e4b88b51712f91863c3b3
Marcus Kida <mkida@atlassian.com>
2014-11-19 17:08:12 +1100

    Initial Commit

---
+++ Colorig/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Colorig
+//
+//  Created by Marcus Kida on 19/11/2014.
+//  Copyright (c) 2014 Marcus Kida. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
